96e9c4836f67b5c63512b9aa8ab695e60cf2b4e6
MertJSX <mertJSX@proton.me>
2026-01-02 21:24:51 +0200

    feat: checkAuth middleware

--- middleware/check_auth.go
+++ middleware/check_auth.go
@@ -1,1 +1,27 @@
 package middleware
+
+import (
+	"github.com/MertJSX/fastvey-server/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+func CheckAuth(c *fiber.Ctx) error {
+	var body map[string]interface{}
+
+	err := c.BodyParser(&body)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"err": "Bad request"})
+	}
+
+	token := c.Get("Authorization")
+
+	if token != "" {
+		_, err := utils.VerifyToken(token, utils.Config.JWTSecret)
+		if err != nil {
+			return c.Status(401).JSON(fiber.Map{"err": "invalid token"})
+		}
+	}
+
+	return c.Next()
+}
