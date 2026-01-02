ab92f1274b65de3fefd84cd452bc194cc9371cc4
MertJSX <mertJSX@proton.me>
2026-01-02 21:29:52 +0200

    refactor(client): Question type

--- client/src/types/Question.ts
+++ client/src/types/Question.ts
@@ -1,10 +1,10 @@
 export interface Question {
     id?: number,
-    surveyId: number,
+    // surveyId?: number, // no need in client
     questionText: string,
     image: string,
-    questionType: string,
+    // questionType: string, // existing only in server/database, it's useless in client (for now)
     minLabel: string,
     maxLabel: string,
-    displayOrder: string
+    // displayOrder?: number // existing only in server/database, it's useless in client
 }
