43d52109b8c2a025a8ee510e65ce9d8831404dcd
MertJSX <mertJSX@proton.me>
2026-01-02 21:33:48 +0200

    feat(ui): init Home page

---
+++ client/src/components/GeneralStats.tsx
@@ -0,0 +1,13 @@
+
+const GeneralStats = () => {
+  return (
+    <section className="w-2/3 flex flex-wrap p-2">
+        <div className="text-3xl bg-gray-700 border-pink-400 p-4 rounded-xl w-[calc(50%-8px)] m-1">Surveys: 12</div>
+        <div className="text-3xl bg-gray-700 border-pink-400 p-4 rounded-xl w-[calc(50%-8px)] m-1">Participants: 278</div>
+        <div className="text-3xl bg-gray-700 border-pink-400 p-4 rounded-xl w-[calc(50%-8px)] m-1">Something: 27</div>
+        <div className="text-3xl bg-gray-700 border-pink-400 p-4 rounded-xl w-[calc(50%-8px)] m-1">Something: 27</div>
+    </section>
+  )
+}
+
+export default GeneralStats
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import SurveysList from "../components/SurveysList"
+import GeneralStats from "../components/GeneralStats"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex gap-2 px-2">
+      <GeneralStats />
+      <SurveysList />
+    </div>
   )
 }
 
